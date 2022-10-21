702d989f4bb321953673b281c4c297780ae629a8
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-21 08:57:10 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { ViewemployessComponent } from './viewemployess/viewemployess.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddemployeeComponent
+    AddemployeeComponent,
+    ViewemployessComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewemployess/viewemployess.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewemployess',
+  templateUrl: './viewemployess.component.html',
+  styleUrls: ['./viewemployess.component.css']
+})
+export class ViewemployessComponent implements OnInit {
+
+  constructor() { }
+
+
+  
+  ngOnInit(): void {
+  }
+
+}
